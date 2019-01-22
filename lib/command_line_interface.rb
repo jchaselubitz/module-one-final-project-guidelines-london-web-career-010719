4a34cd6542e6336f85718141e1bfcaa724eb9fc7
github username <github email address>
2019-01-22 15:47:57 +0000

    developed drink options list

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -7,16 +7,12 @@
 def get_user_name
   puts "Welcome to the best Bar in the World!"
   puts "please enter your name"
-  user = User.create(name: gets.strip)
+  @@session_user = User.create(name: gets.strip)
   # binding.pry
-  puts "Hello #{user.name}, how can I help you?"
+  puts "Hello #{@@session_user.name}, how can I help you?"
 end
 
-
-
-
 #---------------------Main Menu--------------------
-
 
 def main_menu
 puts "1| Search for Drink"
@@ -34,7 +30,6 @@ user_selection = gets.strip.to_i
   # binding.pry
 end
 
-
 #---------------------Drinks Selection--------------------
 
 def get_user_drink
@@ -45,14 +40,6 @@ def get_user_drink
   puts "#{drink_name} is an excellent choice! do you want to know anything about this?"
   drinks_options(drink_name, drink_hash)
 end
-
-
-
-
-
-
-
-
 
 #---------------------Drinks Options--------------------
 
@@ -77,18 +64,33 @@ user_selection = gets.strip.to_i
     puts catagory
     drinks_options(drink_name, drink_hash)
   when 4
-    drink = Cocktail.create(drink_name)# needs work
+    create_cocktail_details(drink_name, drink_hash)
   when 5
     get_user_drink
   end
-   binding.pry
+end
+
+def create_cocktail_details(drink_name, drink_hash)
+idDrink = get_drink_ID_from_api(drink_hash)
+catagory = get_drink_catagory_from_api(drink_hash)
+ingredients = get_drink_ingredients_from_api(drink_hash)
+instructions = get_drink_instructions_from_api(drink_hash)
+create_cocktail(drink_name, idDrink, catagory, instructions, ingredients)
 end
 
 
+def create_cocktail(drink_name, idDrink, catagory, instructions, ingredients)
+  user_drink = Cocktail.create(name: drink_name, user_id: @@session_user.id, idDrink: idDrink, strIBA: catagory, strInstructions: instructions)
+  create_ingredients(user_drink.id, ingredients)
+end
 
-
-
-
+def create_ingredients(id, ingredients)
+  puts "hi!"
+ingredients.each do |ingredient|
+  Ingredient.create(name: ingredient, drink_id: id)
+end
+binding.pry
+end
 
 
 #---------------------Ask Bartender--------------------
