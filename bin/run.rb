99b52109bfad79f3918e8147dc18783e7b91956f
github username <github email address>
2019-01-22 13:46:57 +0000

    arranged command line interface into methods

--- bin/run.rb
+++ bin/run.rb
@@ -11,21 +11,22 @@ require_relative "../apps/modules/ingredient.rb"
 
 def run
 # user = get_user_name
-user = create_user_instance
-user_drink = get_user_drink(user)
-drink_hash = get_drink_hash_by_name(user_drink)
-drink_name = get_drink_name_from_api(drink_hash)
-drink_id = get_drink_ID_from_api(drink_hash)
-drink_catagory = get_drink_catagory_from_api(drink_hash)
-drink_instructions = get_drink_instructions_from_api(drink_hash)
-drink_ingredients = get_drink_ingredients_from_api(drink_hash)
-
-
-drink_ingredients
-binding.pry
-# movies_list = get_character_movies_from_api(character_info)
-# movies_info_hash = get_film_info_for_film(movies_list)
-# print_movies(movies_info_hash)
+user = get_user_name
+main_menu
+# user_drink = get_user_drink
+# drink_hash = get_drink_hash_by_name(user_drink)
+# drink_name = get_drink_name_from_api(drink_hash)
+# drink_id = get_drink_ID_from_api(drink_hash)
+# drink_catagory = get_drink_catagory_from_api(drink_hash)
+# drink_instructions = get_drink_instructions_from_api(drink_hash)
+# drink_ingredients = get_drink_ingredients_from_api(drink_hash)
+#
+#
+# drink_ingredients
+# binding.pry
+# # movies_list = get_character_movies_from_api(character_info)
+# # movies_info_hash = get_film_info_for_film(movies_list)
+# # print_movies(movies_info_hash)
 end
 
 run
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -2,22 +2,76 @@
 #   puts "Welcome to the best Bar in the World!"
 # end
 
+
+#---------------------Start new program--------------------
 def get_user_name
   puts "Welcome to the best Bar in the World!"
   puts "please enter your name"
-  username = gets.strip
-  puts "Hello #{username}!"
-  username
+  user = User.create(name: gets.strip)
+  # binding.pry
+  puts "Hello #{user.name}, how can I help you?"
 end
 
-def create_user_instance
-   user = get_user_name
-   user = User.new(name: user)
-   user.save
-   binding.pry
+
+
+
+#---------------------Main Menu--------------------
+
+
+def main_menu
+puts "1| Search for Drink"
+puts "2| Ask Bartender"
+puts "3| Leave Bar"
+user_selection = gets.strip.to_i
+  case user_selection
+  when 1
+    get_user_drink
+  when 2
+    ask_bartender
+  when 3
+    leave_bar
+  end
+  # binding.pry
 end
 
-def get_user_drink(user)
+
+#---------------------Drinks Selection--------------------
+
+def get_user_drink
   puts "what drink would you like?"
   gets.strip.downcase
+end
+
+
+
+
+
+
+
+
+
+#---------------------Drinks Options--------------------
+
+
+
+
+
+
+
+
+
+
+#---------------------Ask Bartender--------------------
+
+def ask_bartender
+puts "go away!"
+end
+
+
+#---------------------leave bar--------------------
+
+
+
+def leave_bar
+puts "See ya!"
 end
