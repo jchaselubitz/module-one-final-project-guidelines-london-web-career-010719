3187d25c8015d6484e726b97bb6b88a367b5d81b
jchaselubitz <jchaselubitz@gmail.com>
2019-01-25 10:52:25 +0000

    Encapsulated joke teller

--- lib/helper_methods.rb
+++ lib/helper_methods.rb
@@ -36,3 +36,15 @@ end
       puts ""
     end
   end
+
+
+  def joke_teller
+    joke_array = [
+      "An SEO expert walks into a bar, bars, pub, tavern, public house, Irish pub, drinks, beer, alcohol",
+      "How does a computer get drunk?...      It takes screenshots.",
+      "Forget about the past, you can't change it. Forget about the future, you can't predict it. Forget about the present, I didn't get you one.",
+      "What do you call a grilled cheese sandwich that gets right up in your face? Too close for comfort food."
+    ]
+    output = joke_array[rand(0..joke_array.length-1)]
+    output.print_slowly
+  end
